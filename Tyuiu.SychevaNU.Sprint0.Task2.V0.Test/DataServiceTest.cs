d057b792ef0bc1236b0fd486d6141bd52e3df515
Анастасия Сычева <sychevanastia42@gmail.com>
2025-11-30 00:12:52 +0500

    Task2

---
+++ Tyuiu.SychevaNU.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SychevaNU.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SychevaNU.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+
+            var name = "Анастасия";
+            var res = DataService.GetMessage(name);
+
+
+            Assert.AreEqual("Привет..., Анастасия", res);
+        }
+    }
+}
